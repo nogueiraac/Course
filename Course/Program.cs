b5fdb8943d52fe179284ce572084f0a61a9958df
Arthur Cesar F S Nogueira <arthur.nogueira@fitbank.com.br>
2022-05-27 10:04:05 -0300

    Praticing upcasting and downcasting.

--- Course/Program.cs
+++ Course/Program.cs
@@ -134,9 +134,34 @@ namespace Course {
 
         static void Inheritance()
         {
-            BusinessAccount account = new BusinessAccount(8010, "Arthur Cesar", 100.00, 500.0);
+            BusinessAccount bAccount = new BusinessAccount(8010, "Arthur Cesar", 100.00, 500.0);
+            Account acc = new Account(1001, "Anderson", 0.0);
 
-            Console.WriteLine(account.Balance);
+            //UPCASTING
+            Account acc1 = bAccount;
+            Account acc2 = new BusinessAccount(1003, "Isabelle", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(1004, "Cristiano", 0.0, 0.01);
+
+            //DOWNCASTING
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.00);
+
+            //BusinessAccount acc5 = (BusinessAccount)acc3;
+            if(acc3 is BusinessAccount)
+            {
+                //BusinessAccount acc5 = (BusinessAccount)acc3;
+                BusinessAccount acc5 = acc3 as BusinessAccount;
+                acc5.Loan(200);
+                Console.WriteLine("Loan!");
+            }
+
+            if(acc3 is SavingsAccount)
+            {
+                //SavingsAccount acc5 = (SavingsAccount)acc3;
+                SavingsAccount acc5 = acc3 as SavingsAccount;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
         }
 
         static void Main(string[] args) {
